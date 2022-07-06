64ccd503e729397222beb1edf49d0929bdea1e04
LakSK98 <lakshitha199809@gmail.com>
2022-07-06 21:13:14 +0530

    StatisticsControllerAdded

---
+++ WM_Attendance_System/Controllers/StatisticsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WM_Attendance_System.Data;
+using WM_Attendance_System.Models;
+
+namespace WM_Attendance_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly Hybrid_Attendance_SystemContext _context;
+
+        public StatisticsController(Hybrid_Attendance_SystemContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GetChartData(Statistics statistics)
+        {
+            int attendDates = await _context.Attendances
+                .Where(a => a.Date <= statistics.EndDate && a.Date >= statistics.StartDate && a.Type == "Attend" && a.UId == statistics.UId)
+                .CountAsync();
+            int leaveRequests = await _context.LeaveDetails
+                .Where(l => l.Date <= statistics.EndDate && l.Date >= statistics.StartDate && l.SenderId == statistics.UId)
+                .CountAsync();
+            float leaveDuration = await _context.LeaveDetails
+                .Where(l => l.Date <= statistics.EndDate && l.Date >= statistics.StartDate && l.SenderId == statistics.UId)
+                .SumAsync(l=>l.Duration);
+            return Ok(new { attendDates = attendDates, leaveRequests = leaveRequests, leaveDuration = leaveDuration });
+        }
+    }
+}
---
+++ WM_Attendance_System/Models/Statistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WM_Attendance_System.Models
+{
+    public class Statistics
+    {
+        public string ChartType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int UId { get; set; }
+    }
+}
